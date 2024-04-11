4323a8ad41a219e0e705749cac0b3c7c374c5442
Greyson Jones <rgreysonjones@gmail.com>
2024-04-11 16:30:06 -0400

    Added all basic CRUD functions

--- server/database.py
+++ server/database.py
@@ -26,3 +26,35 @@ async def retrieve_cattle():
     async for animal in cattle_collection.find():
         cattle.append(cattle_helper(animal))
     return cattle
+
+# Add a new animal
+async def add_animal(animal_data: dict) -> dict:
+    animal = await cattle_collection.insert_one(animal_data)
+    new_animal = await cattle_collection.find_one({"_id": animal.inserted_id})
+    return cattle_helper(new_animal)
+
+# Retrieve an animal with a matching ID
+async def retrieve_animal(id: str) -> dict:
+    animal = await cattle_collection.find_one({"_id": ObjectId(id)})
+    if animal:
+        return cattle_helper(animal)
+
+# Update an animal with a matching ID
+async def update_animal(id: str, data: dict):
+    # Return false if we got an empty request body
+    if len(data) < 1:
+        return False
+    animal = await cattle_collection.find_one({"_id": ObjectId(id)})
+    if animal:
+        updated_animal = await cattle_collection.update_one(
+            {"_id": ObjectId(id)}, {"$set": data}
+        )
+        if updated_animal:
+            return True
+        return False
+# Delete an animal from the database
+async def delete_animal(id: str):
+    animal = await cattle_collection.find_one({"_id": ObjectId(id)})
+    if animal:
+        await cattle_collection.delete_one({"_id": ObjectId(id)})
+        return True
