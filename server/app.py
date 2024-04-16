26038ba27ccedfc15bf68d7e44f8e75442ec9f5e
Greyson Jones <rgreysonjones@gmail.com>
2024-04-16 16:34:41 -0400

    added CORS middleware

--- server/app.py
+++ server/app.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from server.routes.animal import router as CattleRouter
 
 app = FastAPI()
 app.include_router(CattleRouter, tags=["Cattle"], prefix="/cattle")
+
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
 @app.get("/", tags=["Root"])
 async def read_root():
